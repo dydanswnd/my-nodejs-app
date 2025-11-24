ed501d265ecede469fc3a1cc22817bceef43a968
Hwangjeongmin <jeongcoding@gmail.com>
2025-11-24 08:51:37 +0000

    Update message

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const http = require('http');
-const server = http.createServer((req,res)=>{
-res.writeHead(200,{'Content-Type':'text/html'});
-res.end('<h1>Hello from GitHub Actions CI/CD!</h1>');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello from CI/CD Pipeline! Updated!\n');
 });
 
-server.listen(3000,()=>{
-console.log('Server runnung on port 3000');
+server.listen(3000, '0.0.0.0', () => {
+  console.log('Server running on port 3000');
 });
